e7a110708f9e0be259be537675bf7d6a6c930e90
Evandro Mureb <evandromureb@gmail.com>
2025-02-05 11:25:02 -0300

    Instalação Mary-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,13 @@ import typo	 from '@tailwindcss/typography';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		 './storage/framework/views/*.php',
+		 './resources/**/*.blade.php',
+		 './resources/**/*.js',
+		 './resources/**/*.vue',
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
     theme: {
         extend: {
             fontFamily: {
@@ -19,6 +20,8 @@ export default {
         },
     },
     plugins: [
-		forms, typo
+		forms,
+		typo,
+		require("daisyui")
 	],
 };
